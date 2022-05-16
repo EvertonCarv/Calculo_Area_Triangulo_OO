cf985a4e8aed386c8445dc6bb96e6c1580e4db87
Everton carvalho <everton.de.carvalho@hotmail.com.br>
2022-05-16 20:08:02 -0300

    Tratamento de entrada nulo

--- Calculo_Area_Triangulo_OO/Program.cs
+++ Calculo_Area_Triangulo_OO/Program.cs
@@ -6,10 +6,10 @@ using Calculo_Area_Triangulo_OO;
 Triangulo objTriangulo = new Triangulo();
 
 Console.WriteLine($"Digite a Base do Triángulo");
-objTriangulo.@base = float.Parse(Console.ReadLine());
+objTriangulo.@base = float.Parse(Console.ReadLine() ?? 0.ToString());
 
 Console.WriteLine($"Digite a Altura do Triángulo");
-objTriangulo.altura = float.Parse(Console.ReadLine());
+objTriangulo.altura = float.Parse(Console.ReadLine() ?? throw new NullReferenceException());
 
 objTriangulo.calculeArea(objTriangulo.@base, objTriangulo.altura);
 Console.WriteLine($"A Área do Triángulo é {objTriangulo.area}");
