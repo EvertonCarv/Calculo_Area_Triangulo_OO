46dda1e10c3f3c1d993c2c8b578eb0b27cbed34b
Everton carvalho <everton.de.carvalho@hotmail.com.br>
2022-05-15 21:21:31 -0300

    Adicionar arquivos de projeto.

---
+++ Calculo_Area_Triangulo_OO/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using Calculo_Area_Triangulo_OO;
+
+Triangulo objTriangulo = new Triangulo();
+
+Console.WriteLine($"Digite a Base do Triángulo");
+objTriangulo.@base = float.Parse(Console.ReadLine());
+
+Console.WriteLine($"Digite a Altura do Triángulo");
+objTriangulo.altura = float.Parse(Console.ReadLine());
+
+objTriangulo.calculeArea(objTriangulo.@base, objTriangulo.altura);
+Console.WriteLine($"A Área do Triángulo é {objTriangulo.area}");
---
+++ Calculo_Area_Triangulo_OO/Triangulo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculo_Area_Triangulo_OO
+{
+    public class Triangulo
+    {
+        public float @base;
+        public float altura;
+        public float area;
+
+        public float calculeArea(float @base, float altura)
+        {
+            return this.area =  (@base * altura) / 2;
+        }
+
+    }
+}
